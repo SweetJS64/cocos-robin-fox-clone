1324e0923a60fd31a6cd23e107b556a3d97f8208
SweetJS64 <miroran64@gmail.com>
2024-10-23 04:29:30 +0400

    first version

---
+++ assets/scripts/scrollingMap.ts
@@ -0,0 +1,21 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class scrollingMap extends cc.Component {
+
+    goToSlot(){
+        let wrapMode = cc.WrapMode.Normal; 
+        this._playScroll(wrapMode);
+    }
+
+    goToHome(){
+        let wrapMode = cc.WrapMode.Reverse; 
+        this._playScroll(wrapMode);
+    }
+    
+    private _playScroll(wrapMode: cc.WrapMode){
+        const anim = this.getComponent(cc.Animation);
+        anim.play('scrollingMainMap').wrapMode = wrapMode; 
+        anim.play('scrollingMainMap');
+    }
+}
