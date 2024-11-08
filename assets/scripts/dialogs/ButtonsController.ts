ad3c220b20816f274680a9b6e0d6a1e6099dc0c3
SweetJS64 <miroran64@gmail.com>
2024-11-09 02:29:20 +0400

    add menu && build in buttonControllers

--- assets/scripts/dialogs/ButtonsController.ts
+++ assets/scripts/dialogs/ButtonsController.ts
@@ -8,19 +8,45 @@ export default class ButtonsController extends cc.Component
 {
     @property(DialogsController)
     dialogsController: DialogsController = null;
+
+    @property(cc.Node)
+    slotTopRightUIBlock: cc.Node = null;
     @property(cc.Node)
     slotBotLeftUIBlock: cc.Node = null;
+    @property(cc.Node)
+    slotBotRightUIBlock: cc.Node = null;
 
-    @property(cc.Prefab)
-    sharingButtonPrefab: cc.Prefab = null;
+    @property(cc.Node)
+    cityTopRightUIBlock: cc.Node = null;
+    @property(cc.Node)
+    cityBotLeftUIBlock: cc.Node = null;
+    @property(cc.Node)
+    cityBotRightUIBlock: cc.Node = null;
+    
+    // @property(cc.SpriteFrame)
+    // slotsButtonSprite: cc.SpriteFrame = null;
+    // @property(cc.SpriteFrame)
+    // cityButtonSprite: cc.SpriteFrame = null;
+
+    @property(cc.SpriteFrame)
+    menuButtonSprite: cc.SpriteFrame = null;
+    @property(cc.SpriteFrame)
+    buildButtonSprite: cc.SpriteFrame = null;    
+    @property(cc.SpriteFrame)
+    sharingButtonSprite: cc.SpriteFrame = null;
 
     protected onLoad() {
-        this.init(this.sharingButtonPrefab, this.dialogsController.sharingDialogController);
+        this.init(this.menuButtonSprite, this.dialogsController.mainMenuController, this.cityTopRightUIBlock);
+        this.init(this.menuButtonSprite, this.dialogsController.mainMenuController, this.slotTopRightUIBlock);
+
+        this.init(this.buildButtonSprite, this.dialogsController.buildDialogController, this.cityBotLeftUIBlock);
+        this.init(this.sharingButtonSprite, this.dialogsController.sharingDialogController,  this.slotBotLeftUIBlock);
     }
 
-    init(buttonPrefab: cc.Prefab, dialogsController: AbstractDialogsController) {
-        let buttonNode = cc.instantiate(buttonPrefab);
-        this.slotBotLeftUIBlock.addChild(buttonNode);
+    init(buttonSprite: cc.SpriteFrame, dialogsController: AbstractDialogsController, UIBlock: cc.Node) {
+        let buttonNode = cc.instantiate(new cc.Node);
+        buttonNode.addComponent(cc.Sprite).spriteFrame = buttonSprite;        
+        UIBlock.addChild(buttonNode);
 
         buttonNode.on('mousedown', function () {
             this._clickButtonAnimation(buttonNode, dialogsController);
